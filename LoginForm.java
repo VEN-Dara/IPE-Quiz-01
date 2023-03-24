ba55546d08b01422a8776eda78432507a9153928
SuosPhearith <suosphearith@gmail.com>
2023-03-24 23:15:48 +0700

    register

--- LoginForm.java
+++ LoginForm.java
@@ -7,12 +7,13 @@ public class LoginForm {
     boolean loginSuccess;
     Scanner sc = new Scanner(System.in);
 
-    public LoginForm(String userName, String gmail, String password ) {
+    public LoginForm(String userName, String gmail, String password) {
         this.userName = userName;
         this.gmail = gmail;
         this.password = password;
     }
-    public LoginForm( ) {
+
+    public LoginForm() {
         this.password = "vendara";
     }
 
@@ -23,23 +24,49 @@ public class LoginForm {
                 ----------------------------------------
                 1. Register Account
                 2. Login Account
-                        *If you already have an account. 
-                3. Exit   
+                        *If you already have an account.
+                3. Exit
                 """);
     }
 
-    //Register Page
+    // Register Page
     public void registerPage() {
-        
+        System.out.print("""
+                ----------------------------------------
+                                Register
+                ----------------------------------------
+                """);
+        System.out.print(" Fisrt Name: ");
+        firstName = sc.nextLine();
+        System.out.print("  Last Name: ");
+        lastName = sc.nextLine();
+        System.out.print("      Gmail: ");
+        gmail = sc.nextLine();
+
+        do {
+            System.out.print("   Password: ");
+            password = sc.nextLine();
+            System.out.print("Re-Password: ");
+            rePassword = sc.nextLine();
+
+            // Compare Password
+            if (rePassword.compareTo(password) < 0 || rePassword.compareTo(password) > 0) {
+                System.out.println("Re-password is wrong!\nTry agian!");
+            }
+        } while (rePassword.compareTo(password) < 0 || rePassword.compareTo(password) > 0);
+        System.out.print("""
+                ----------------------------------------
+                         Register Successfully!
+                ----------------------------------------
+                """);
     }
 
-    //Reset Password
+    // Reset Password
     public void resetPasswordPage() {
-        
-           
+
     }
 
-    //Login Page
+    // Login Page
     public void loginPage() {
         String ans = "N";
         System.out.print("""
@@ -52,40 +79,40 @@ public class LoginForm {
         do {
             System.out.print("Password: ");
             String password = sc.nextLine();
-            //Compare Password
-            if(password.compareTo(this.password) < 0 || password.compareTo(this.password) > 0 ) {
+            // Compare Password
+            if (password.compareTo(this.password) < 0 || password.compareTo(this.password) > 0) {
                 System.out.println("Wrong Password!\nWanna try agian! (Y/N): ");
                 ans = sc.nextLine();
-            }
-            else {
+            } else {
                 System.out.print("""
-                    ----------------------------------------
-                                Login Successfully !
-                    ----------------------------------------
-                    """);
+                        ----------------------------------------
+                                    Login Successfully !
+                        ----------------------------------------
+                        """);
                 loginSuccess = true;
             }
-        }
-        while(ans.compareTo("Y") == 0 || ans.compareTo("y") == 0 );
+        } while (ans.compareTo("Y") == 0 || ans.compareTo("y") == 0);
     }
 
     public void profile() {
         System.out.print("""
-            ----------------------------------------
-                           Profile
-            ----------------------------------------
-            """);
+                ----------------------------------------
+                               Profile
+                ----------------------------------------
+                """);
         System.out.println("             " + lastName + "_" + firstName);
         System.out.println("- Gmail: " + gmail);
         System.out.println("1. Reset Password");
         System.out.println("2. Logout Account");
         System.out.println("3. Exit");
     }
+
     public static void main(String[] args) {
         /*
-        String userName, lastName, fisrtName;
-        String gmail;
-        String password;  */
+         * String userName, lastName, fisrtName;
+         * String gmail;
+         * String password;
+         */
         Scanner sc = new Scanner(System.in);
         int option;
         LoginForm loginForm = new LoginForm();
@@ -94,18 +121,18 @@ public class LoginForm {
             loginForm.landingPage();
             System.out.print("-> Your option: ");
             option = sc.nextInt();
-            switch(option) {
+            switch (option) {
                 case 1:
                     loginForm.registerPage();
                     break;
                 case 2:
                     loginForm.loginPage();
-                    if(loginForm.loginSuccess) {
+                    if (loginForm.loginSuccess) {
                         do {
                             loginForm.profile();
                             System.out.print("-> Your option: ");
                             option = sc.nextInt();
-                            switch(option) {
+                            switch (option) {
                                 case 1:
                                     loginForm.resetPasswordPage();
                                     break;
@@ -118,19 +145,14 @@ public class LoginForm {
                                     System.out.println("Option is not availble!!");
                                     break;
                             }
-                        }
-                        while (option > 3 || option < 2);
+                        } while (option > 3 || option < 2);
                     }
                     break;
                 default:
                     System.out.println("Option is not availble!!");
                     break;
             }
-        }
-        while(option != 3);
-    
-
-
+        } while (option != 3);
 
     }
 }
